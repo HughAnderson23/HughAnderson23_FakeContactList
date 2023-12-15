1818e618031de56eb8112e07b9c32736c21da3e8
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-12-15 20:21:27 +0000

    delete Contact functionality added. working on random picuture generator.

---
+++ src/img/imagePack.jsx
@@ -0,0 +1,19 @@
+import IconKid_1 from "/workspaces/HughAnderson23_FakeContactList/src/img/Icon Kid_1.jpg";
+import IconKid_2 from "/workspaces/HughAnderson23_FakeContactList/src/img/Icon Kid_2.jpg"
+
+
+export const imageNames = () => (
+	<div className="text-center mt-5">
+		
+		<p>
+			<img src={IconKid_1} />
+            <img src={IconKid_2} />
+		</p>
+		
+	
+	</div>
+);
+
+
+
+export default imageNames;
--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -1,21 +1,34 @@
 import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Context } from '../store/appContext';
+import imageNames from '/workspaces/HughAnderson23_FakeContactList/src/img/imagePack.jsx';
+
+
+const getRandomImage = () => {
+  const randomIndex = Math.floor(Math.random() * imageNames.length);
+  return `/workspaces/HughAnderson23_FakeContactList/src/img/${imageNames[randomIndex]}`;
+};
 
 const ContactCard = ({ contactId }) => {
   const{ store, actions} = useContext(Context)
-  
+  const handleDelete = (contact) => {
+    
+    actions.deleteContact(contact);
+  };  
   return (
     <ol>
         {store.contacts.map((contact) => (
           <li key={contact.id}>
             <div className="contact-card">
+            <img  src={getRandomImage()}
+                  style={{ width: '100px', height: '100px' }} 
+                />
                 <h3>{contact.name}</h3>
                 <p>Email: {contact.email}</p>
                 <p>Phone: {contact.phone}</p>
                 <p>Address: {contact.address}</p>
                 {/* Add more details as needed */}
+                <button onClick={() => handleDelete(contact)}>Delete Contact</button>
             </div>
           </li>
         ))}
